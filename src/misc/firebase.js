9f8caf674f92a0166ad20920d7ac6342f5784329
Sauvic <sauvicpaulchy@gmail.com>
2021-10-05 16:31:04 +0530

    Initialized firebase for the app

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCVSYlS_Gqr_W4H6Mel2HzWb8GgRvFphq4',
+  authDomain: 'chat-web-app-7b222.firebaseapp.com',
+  projectId: 'chat-web-app-7b222',
+  storageBucket: 'chat-web-app-7b222.appspot.com',
+  messagingSenderId: '1087819709375',
+  appId: '1:1087819709375:web:5625f33be26c90a8e6ff0a',
+};
+
+const app = firebase.initializeApp(config);
