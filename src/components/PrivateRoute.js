081f0240cda8794d49102892e36d7288e579385a
Sauvic <sauvicpaulchy@gmail.com>
2021-10-05 17:43:56 +0530

    managed user with profile context and checked for realtime subscription

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router';
+import { Container, Loader } from 'rsuite';
+import { useProfile } from '../context/profile.context';
 
 const PrivateRoute = ({ children, ...routeprops }) => {
-  const profile = false;
+  const { profile, isLoading } = useProfile();
 
-  if (!profile) {
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (!profile && !isLoading) {
     return <Redirect to="/signin" />;
   }
   return <Route {...routeprops}> {children}</Route>;
