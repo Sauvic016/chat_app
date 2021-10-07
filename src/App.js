2ba00451d09ae285b04e1b349108a5e87c83ef7d
Sauvic <sauvicpaulchy@gmail.com>
2021-10-07 22:09:13 +0530

    created layout for home page and created chat page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './styles/main.scss';
 import { Switch } from 'react-router';
 import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute';
-import Home from './pages/Home';
+import Home from './pages/Home/index';
 import PublicRoute from './components/PublicRoute';
 import { ProfileProvider } from './context/profile.context';
 
